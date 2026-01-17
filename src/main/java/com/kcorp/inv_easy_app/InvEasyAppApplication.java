91024b0865f018a3624f98c23fad658f831a1718
kcdriod <ch.chaitanya8989@gmail.com>
2026-01-17 18:24:20 -0500

    Initial commit

---
+++ src/main/java/com/kcorp/inv_easy_app/InvEasyAppApplication.java
@@ -0,0 +1,13 @@
+package com.kcorp.inv_easy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvEasyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvEasyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kcorp/inv_easy_app/InvEasyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcorp.inv_easy_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvEasyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
